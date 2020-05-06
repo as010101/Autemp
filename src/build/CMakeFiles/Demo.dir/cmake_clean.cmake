2a3ae6b536ab8a13afebba3e9be02eeae27e4713
root <root@localhost.localdomain>
2020-05-06 04:11:32 -0400

    up

---
+++ src/build/CMakeFiles/Demo.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Demo.dir/AggregateState.cpp.o"
+  "CMakeFiles/Demo.dir/BufferList.cpp.o"
+  "CMakeFiles/Demo.dir/GroupByBox.cpp.o"
+  "CMakeFiles/Demo.dir/GroupByHash.cpp.o"
+  "CMakeFiles/Demo.dir/GroupByState.cpp.o"
+  "CMakeFiles/Demo.dir/Hash.cpp.o"
+  "CMakeFiles/Demo.dir/HashForAggregate.cpp.o"
+  "CMakeFiles/Demo.dir/HashForBufferList.cpp.o"
+  "CMakeFiles/Demo.dir/HashForNewState.cpp.o"
+  "CMakeFiles/Demo.dir/HashWithList.cpp.o"
+  "CMakeFiles/Demo.dir/List.cpp.o"
+  "CMakeFiles/Demo.dir/main.cpp.o"
+  "CMakeFiles/Demo.dir/NewState.cpp.o"
+  "CMakeFiles/Demo.dir/Parse.cpp.o"
+  "CMakeFiles/Demo.dir/SortList.cpp.o"
+  "CMakeFiles/Demo.dir/State.cpp.o"
+  "CMakeFiles/Demo.dir/TrashHash.cpp.o"
+  "CMakeFiles/Demo.dir/TrashState.cpp.o"
+  "Demo.pdb"
+  "Demo"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
